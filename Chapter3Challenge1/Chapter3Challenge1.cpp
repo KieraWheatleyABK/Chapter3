789303e368214feaff6edecaeb8540ae4bdc02c6
Kiera Wheatley <kiera.wheatley@activision.com>
2022-07-19 14:58:39 -0400

    final solution

--- Chapter3Challenge1/Chapter3Challenge1.cpp
+++ Chapter3Challenge1/Chapter3Challenge1.cpp
@@ -2,19 +2,59 @@
 //
 
 #include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int GetCurrentYear();
+bool IsLeapYear(int year);
+void DisplayYearInfo();
+
+int GetCurrentYear()
+{
+    struct tm newtime;
+    time_t now = time(0);
+    localtime_s(&newtime, &now);
+
+    return 1900 + newtime.tm_year;;
+}
+
+bool IsLeapYear(int year)
+{
+    if (year % 4 == 0)
+    {
+        return true;
+    }
+    else if (year % 400 == 0)
+    {
+        return true;
+    }
+    else if (year % 100 == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void DisplayYearInfo()
+{
+    int currentYear = GetCurrentYear();
+    cout << "The current year is " << currentYear << "." << endl;
+
+    if (IsLeapYear(currentYear))
+    {
+        cout << "This year is a leap year!" << endl;
+    }
+    else
+    {
+        cout << "This year is not a leap year." << endl;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    DisplayYearInfo();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
